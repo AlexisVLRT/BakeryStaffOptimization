7c6bf8e77e530834c3eaa986e1c2ccb0ab91aca5
Ugdha <alexisvlrt@gmail.com>
2018-06-20 17:25:39 +0200

    First take at a structure

---
+++ Schedule.py
@@ -0,0 +1,6 @@
+class Schedule:
+    def __init__(self):
+        pass
+
+    def get_fitness(self):
+        return 0
---
+++ Schedule_optimizer.py
@@ -0,0 +1,3 @@
+class Scheduler:
+    def __init__(self, input_data=None):
+        self.input_data = input_data
